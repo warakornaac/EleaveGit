df350ade7293337003b3cfa84a915334e37bdf47
warakornaac <warakorn.pra@aac.co.th>
2024-12-02 17:23:01 +0700

    [view&controller] update from approve

---
+++ Eleave.Models/StoreGetLogApprove.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eleave.Models
+{
+    public class StoreGetLogApprove
+    {
+        public string ReqID { get; set; }
+        public string ApprStep { get; set; }
+        public string ApproveDate { get; set; }
+        public string ApproveBy { get; set; }
+        public string ApproveByName { get; set; }
+        public string ApproveEmail { get; set; }
+        public string RequestStatus { get; set; }
+        public string RequestStatusName { get; set; }
+        public string ApprComment { get; set; }
+    }
+}
