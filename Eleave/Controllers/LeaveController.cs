f8a5ed06131cdfbd75580c936e9abb2a461c5e7e
ArmAAC <armoop03@gmail.com>
2025-02-07 11:51:38 +0700

    [bin&controller&view&config] request add label attach file

--- Eleave/Controllers/LeaveController.cs
+++ Eleave/Controllers/LeaveController.cs
@@ -484,7 +484,7 @@ namespace Eleave.Controllers
             string message = string.Empty;
             try
             {
-                LeaveType = new GetLeaveType().GetLeaveTypeList();
+                LeaveType = new GetLeaveType().GetLeaveTypeList("");
                 message = "Y";
             }
             catch (Exception ex)
